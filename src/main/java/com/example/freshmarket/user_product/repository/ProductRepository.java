650ef8e14661926f3a2ba1023a73b88adc263175
uhjin1130 <rladjwls1130@naver.com>
2026-07-02 17:34:34 +0900

    feat: 상품 조회 Entity/Repository/Service 구현

--- src/main/java/com/example/freshmarket/user_product/repository/ProductRepository.java
+++ src/main/java/com/example/freshmarket/user_product/repository/ProductRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.Instant;
 import java.util.Optional;
 
 @Repository
